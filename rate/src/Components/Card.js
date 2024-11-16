7463a6b77074c2a92a5c90066563722718fd6360
Laxman Yadav <125170892+laxmany9335@users.noreply.github.com>
2024-11-16 14:53:27 +0000

    add tailwind

--- rate/src/Components/Card.js
+++ rate/src/Components/Card.js
@@ -1,39 +1,43 @@
 import React from 'react'
 import { FaQuoteLeft,FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft,FiChevronRight } from "react-icons/fi";
 function Card(props) {
     let review = props.review;
   return (
-    <div>
-        <div>
-            <img src={review.image}/>
+    <div className='flex flex-col md:relative'>
+        <div className='absolute top-[-7rem] z-[10] mx-auto'>
+            <img
+            className='aspect-sqaure rounded-full w-[140px] h-[140px] z-[25]'
+            src={review.image}/>
+            <div className='w-[140px] h-[140px] rounded-full bg-violet-500 absolute top-[-6px] z-[-20] left-[10px] '></div>
         </div>
 
-        <div>
-            <p>{review.name}</p>
+        <div className='text-center mt-7'>
+            <p className='font-bold text-2xl capitalize'>{review.name}</p>
         </div>
            
-        <div>
-            <p>{review.job }</p>
+        <div className='text-center mt-7'>
+            <p className='text-violet-300 uppercase text-sm'>{review.job }</p>
         </div>
 
-        <div>
+        <div className='text-violet-400 mx-auto mt-5'>
         <FaQuoteLeft />
         </div>
 
-        <div>
+        <div className='text-center mt-4 text-slate-500'>
             {review.text}
         </div>
 
-        <div>
+        <div className='text-violet-400 mx-auto mt-5'>
         <FaQuoteRight />
         </div>
 
-        <div>
-            <button>
-                +
+        <div className='flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto'> 
+            <button className='cursor-pointer hover:text-violet-500'>
+            <FiChevronLeft />
             </button>
-            <button>
-                *
+            <button className='cursor-pointer hover:text-violet-500'>
+            <FiChevronRight />
             </button>
         </div>
         <div>
